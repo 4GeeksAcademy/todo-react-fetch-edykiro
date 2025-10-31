7d9d89c069eaa724538b0531b065b08cec60383a
Eduard Quiroga <edykiro@gmail.com>
2025-10-31 14:43:03 +0100

    Commit semifinal

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,22 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { createUser, getAllUsers, getUserData, deleteUser } from "../services";
+import { use } from "react";
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [users, setUsers] = useState([]);
+  const [userTasks, setUserTasks] = useState([]);
+  const [user, createUserVariable] = useState("");
+
+  const handleChange = async (event) => {
+    const userName = event.target.value;
+    const fetchData = await getUserData(userName);
+    createUserVariable(userName);
+    setUserTasks(fetchData.todos);
+  };
 
   const handleKeyDown = (event) => {
     if (event.key === "Enter" && inputValue.trim() !== "") {
-      setTasks((previous) => [...previous, inputValue.trim()]);
-      setInputValue("");
+      //THIS WILL BE FOR ADDING THE TASKS
+
+      console.log(users);
     }
   };
 
   const editArray = (index) => {
-    const newTasks = tasks.filter((task) =>  task !== tasks[index]);
-    setTasks(newTasks);
-
-
+    const newTasks = tasks.filter((task) => task !== tasks[index]);
   };
+
+  useEffect(() => {
+    getAllUsers(setUsers);
+    setInputValue("");
+  }, [users]);
+
+  useEffect(() => {
+    getAllUsers(setUsers);
+    setInputValue("");
+  },[]);
+
+  console.log(user);
 
   return (
     <>
@@ -33,23 +55,68 @@ const App = () => {
             />
           </div>
           <ul className="list-group">
-            {tasks.length > 0 ? tasks.map((task,index) => (
-              <li className="list-group-item d-flex justify-content-between">
-                {task}{" "}
-                <button onClick={()=>editArray(index)}
-                  className="btn-delete-task border-0 bg-white"
-                  title="Eliminar tarea"
-                >
-                  X
-                </button>
+            {tasks.length > 0 ? (
+              tasks.map((task, index) => (
+                <li className="list-group-item d-flex justify-content-between">
+                  {task}{" "}
+                  <button
+                    onClick={() => editArray(index)}
+                    className="btn-delete-task border-0 bg-white"
+                    title="Eliminar tarea"
+                  >
+                    X
+                  </button>
+                </li>
+              ))
+            ) : (
+              <li className="list-group-item text-center ">
+                No tasks left, add one
               </li>
-            )) 
-          : 
-          <li className="list-group-item text-center ">No tasks left, add one</li>
-          }
-
+            )}
           </ul>
         </div>
+      </div>
+
+      <div className="d-flex col-6">
+        <div className="col-6 mx-3">
+          <div className="input-group">
+            <input
+              onChange={({ target }) => createUserVariable(target.value)}
+              type="text"
+              className="form-control"
+              placeholder="Username"
+            />
+            <button
+              onClick={() => createUser(user)}
+              className="btn btn-success mx-1"
+            >
+              <i className="fa-solid fa-plus"></i>
+            </button>
+          </div>
+        </div>
+        <select
+          onChange={handleChange}
+          className="form-select"
+          aria-label="Default select example"
+        >
+          <option defaultValue="0">Open this select menu </option>
+          {users.map((user) => (
+            <option value={user.name}>{user.name}</option>
+          ))}
+        </select>
+        <button
+          onClick={() => deleteUser(user)}
+          className="btn btn-danger mx-1"
+        >
+          <i className="fa-solid fa-trash"></i>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {userTasks.map((element) => (
+            <li>{element.label}</li>
+          ))}
+        </ul>
       </div>
     </>
   );
--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -8,18 +8,9 @@ const Navbar = () => {
       <nav className="navbar bg-body-tertiary">
         <div className="container">
           <a className="navbar-brand">Todo-list</a>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+
         </div>
+        
       </nav>
     </>
   );
