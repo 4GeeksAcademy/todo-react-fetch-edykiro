6178469d396db9718de77c45e82b15ec4dadf6dd
Eduard Quiroga <edykiro@gmail.com>
2025-11-04 16:19:57 +0100

    Test

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -77,40 +77,7 @@ const App = () => {
         </div>
       </div>
 
-      <div className="d-flex col-6">
-        <div className="col-6 mx-3">
-          <div className="input-group">
-            <input
-              onChange={({ target }) => createUserVariable(target.value)}
-              type="text"
-              className="form-control"
-              placeholder="Username"
-            />
-            <button
-              onClick={() => createUser(user)}
-              className="btn btn-success mx-1"
-            >
-              <i className="fa-solid fa-plus"></i>
-            </button>
-          </div>
-        </div>
-        <select
-          onChange={handleChange}
-          className="form-select"
-          aria-label="Default select example"
-        >
-          <option defaultValue="0">Open this select menu </option>
-          {users.map((user) => (
-            <option value={user.name}>{user.name}</option>
-          ))}
-        </select>
-        <button
-          onClick={() => deleteUser(user)}
-          className="btn btn-danger mx-1"
-        >
-          <i className="fa-solid fa-trash"></i>
-        </button>
-      </div>
+ 
       <div>
         <ul>
           {userTasks.map((element) => (
--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -2,15 +2,48 @@ import React from "react";
 import { use } from "react";
 import { useState } from "react";
 
+
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar bg-body-tertiary">
+      <nav className="navbar bg-body-secondary">
         <div className="container">
           <a className="navbar-brand">Todo-list</a>
-
         </div>
-        
+        <div className="d-flex col-6">
+          <div className="col-6 mx-3">
+            <div className="input-group">
+              <input
+                onChange={({ target }) => createUserVariable(target.value)}
+                type="text"
+                className="form-control"
+                placeholder="Username"
+              />
+              <button
+                onClick={() => createUser(user)}
+                className="btn btn-success mx-1"
+              >
+                <i className="fa-solid fa-plus"></i>
+              </button>
+            </div>
+          </div>
+          <select
+            onChange={handleChange}
+            className="form-select"
+            aria-label="Default select example"
+          >
+            <option defaultValue="0">Open this select menu </option>
+            {users.map((user) => (
+              <option value={user.name}>{user.name}</option>
+            ))}
+          </select>
+          <button
+            onClick={() => deleteUser(user)}
+            className="btn btn-danger mx-1"
+          >
+            <i className="fa-solid fa-trash"></i>
+          </button>
+        </div>
       </nav>
     </>
   );
